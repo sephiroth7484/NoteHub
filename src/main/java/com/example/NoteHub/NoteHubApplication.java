2c4a1e5c8768e00127edd176f2e4a6d7d1ac5452
Sushant Pandey <85762925+sephiroth7484@users.noreply.github.com>
2024-10-08 22:14:01 +0530

    Intial Commit

---
+++ src/main/java/com/example/NoteHub/NoteHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.NoteHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NoteHub/NoteHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NoteHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
